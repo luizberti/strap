4ee279563108078f9ddf226a490a8f1cdba05629
Luiz Berti <luizberti@users.noreply.github.com>
2018-12-24 20:57:17 -0200

    compartmentised aliases

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -1,6 +1,10 @@
+# FILESYSTEM
+# filesystem navigation, listing, and operating
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
+alias .....='cd ../../../..'
 
 alias ll='ls -l'
 alias lsa='ls -A'
@@ -8,10 +12,16 @@ alias lla='ls -lA'
 alias tra="tree -a -F -I '.git|.hg|.DS_Store|.sass-cache'"
 alias tre='tree --dirsfirst -F'
 
-alias gs='git status'
-alias gb='git branch'
-alias gc='git checkout'
 
+# GIT
+# git shorthands and utils
+
+alias gs='git status'
 alias tiga='tig --all'
+
+
+# MISC
+# miscellaneous cli shortcuts
+
 alias ccat='pygmentize -O style=monokai -f console256 -g'
 
