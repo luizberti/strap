f8c1db619d5c76cea367b0223312788bfb8c4596
Luiz Berti <luizberti@users.noreply.github.com>
2018-12-24 23:03:02 -0200

    added shopts

--- dotfiles/profile.sh
+++ dotfiles/profile.sh
@@ -3,6 +3,7 @@
 # SHELL ENVIRONMENT
 export LANG=en_US LC_ALL=en_US.UTF-8 LC_CTYPE=UTF-8
 export CLICOLOR=1 EDITOR=vim
+shopt -s globstar extglob
 PS1='\e[0;36m\t  \e[35m\u@\h  \e[33m\w  \e[31m[$?]\e[0m\n\$ '
 
 # PATH MANGLING
