ac261841d1e6f5babcd37131f74fa96d32b920da
Luiz Berti <luizberti@users.noreply.github.com>
2019-07-17 02:01:16 -0300

    nvim is now default

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -1,6 +1,3 @@
-# FILESYSTEM
-# filesystem navigation, listing, and operating
-
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
@@ -12,16 +9,10 @@ alias lla='ls -lA'
 alias tra="tree -a -F -I '.git|.hg|.DS_Store|.sass-cache'"
 alias tre='tree --dirsfirst -F'
 
-
-# GIT
-# git shorthands and utils
-
 alias gs='git status'
 alias tiga='tig --all'
 
-
-# MISC
-# miscellaneous cli shortcuts
-
+[ -x "$(command -v nvim)" ] && alias vim='nvim'
+[ -x "$(command -v nvim)" ] && alias vimdiff='nvim -d'
 alias ccat='pygmentize -O style=monokai -f console256 -g'
 
