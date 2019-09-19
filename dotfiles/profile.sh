4f9aa7f71a1acc3c4c998a1594a856e2ef90f26c
Luiz Berti <luizberti@users.noreply.github.com>
2019-09-19 18:42:42 -0300

    better prompt

--- dotfiles/profile.sh
+++ dotfiles/profile.sh
@@ -4,10 +4,30 @@
 export LANG=en_US LC_ALL=en_US.UTF-8 LC_CTYPE=UTF-8
 export CLICOLOR=1 EDITOR=vim
 shopt -s globstar extglob
-PS1='\e[0;36m\t  \e[35m\u@\h  \e[33m\w  \e[31m[$?]\e[0m\n\$ '
+PS1='\e[0;36m\t  \e[38;5;204m\u\e[38;5;242m@\e[38;5;212m\h.local  \e[33m\w\e[38;5;121m$PROMPT_GIT\e[31m$PROMPT_EXIT\e[38;5;75m$PROMPT_ELAPSED\e[0m\n\$ '
 HISTSIZE=1000
 HISTFILESIZE=10000
 HISTCONTROL=ignoredups:ignorespace
+HISTTIMEFORMAT='%s  '
+PROMPT_COMMAND="$(cat <<EOF
+PROMPT_EXIT=\$?
+test \$PROMPT_EXIT -eq 0 && unset PROMPT_EXIT || PROMPT_EXIT="  ⌦ \$PROMPT_EXIT"
+
+# HISTCONTROL=ignoredups MAY SCREW WITH THIS TIMER
+PROMPT_ELAPSED=\$[\$(date +%s)-\$(history 1 | awk 'NR==1 {print \$2}')]
+test \$PROMPT_ELAPSED -lt 8 && unset PROMPT_ELAPSED || PROMPT_ELAPSED="  Δ \${PROMPT_ELAPSED}s"
+
+# ROOT COLOR
+
+# GIT INTEGRATION
+PROMPT_GIT="  \$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+if test \$? -eq 0; then
+    :
+else
+    unset PROMPT_GIT
+fi
+EOF
+)"
 
 # PATH MANGLING
 export PATH=$HOME/.cargo/bin:/usr/local/bin:/usr/local/sbin:$PATH
